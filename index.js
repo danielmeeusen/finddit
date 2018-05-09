da7e03bfb258194fe0d6173a81f8b2eaf4d523ff
Daniel Meeusen <danmeeusen@gmail.com>
2018-05-08 19:06:43 -0700

    start

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert(123);
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', e => {
+   // Get search term
+   const searchTerm = searchInput.value;
+   // get sort
+   const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+   // get limit
+   const searchLimit = document.getElementById('limit').value;
+   console.log(searchLimit);
+   e.preventDefault();
+});
